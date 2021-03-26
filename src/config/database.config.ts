939c4972752902ce5312e5b51e11d04023721c2b
Maodo <maodosw@gmail.com>
2021-03-26 13:08:05 +0000

    Some configs addition

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,5 +1,3 @@
 export default () => ({
-  mongoUrl:
-    process.env.MONGODB_URL ||
-    'mongodb+srv://maodo:nestjsdb@cluster0.sdx6d.mongodb.net/nestjsdb?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGODB_URL,
 });
